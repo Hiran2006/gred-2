c9c5b9faf5c3f2260f8fe90351ba6fe14e0a2632
Hiran S <hirans2006@gmail.com>
2025-11-27 18:24:00 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+"use client"
+import "./global.css"
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head></head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='bg-white dark:bg-black dark: w-screen h-screen'>
+      <div>
+        <h2>GRED</h2>
+      </div>
+    </div>
+  )
+}
